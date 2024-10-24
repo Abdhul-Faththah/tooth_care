246942f16d2e433be9359752f4ee9cec68506549
Abdhul-Faththah <abdhulfaththah1@gmail.com>
2024-10-24 11:00:12 -0500

    modify

---
+++ views/admin/user.php
@@ -0,0 +1,22 @@
+<?php
+require_once('../layouts/header.php');
+?>
+
+<!-- Content -->
+
+<div class="container-xxl flex-grow-1 container-p-y">
+    <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Dashboard /</span> User</h4>
+
+
+
+    <hr class="my-5" />
+
+
+</div>
+<!--/ Responsive Table -->
+</div>
+<!-- / Content -->
+
+<?php
+require_once('../layouts/footer.php');
+?>
--- views/layouts/login_footer.php
+++ views/layouts/login_footer.php
@@ -1,12 +1,3 @@
-</div>
-<!-- / Layout page -->
-</div>
-
-<!-- Overlay -->
-<div class="layout-overlay layout-menu-toggle"></div>
-</div>
-<!-- / Layout wrapper -->
-
 <!-- Core JS -->
 <!-- build:js assets/vendor/js/core.js -->
 <script src="<?= asset('assets/vendor/libs/jquery/jquery.js') ?>"></script>
@@ -18,16 +9,11 @@
 <!-- endbuild -->
 
 <!-- Vendors JS -->
-<script src="<?= asset('assets/vendor/libs/apex-charts/apexcharts.js') ?>"></script>
 
 <!-- Main JS -->
 <script src="<?= asset('assets/js/main.js') ?>"></script>
 
 <!-- Page JS -->
-<script src="<?= asset('assets/js/dashboards-analytics.js') ?>"></script>
-
-<!-- Place this tag in your head or just before your close body tag. -->
-<script async defer src="https://buttons.github.io/buttons.js"></script>
 </body>
 
 </html>
